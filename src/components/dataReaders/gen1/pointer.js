f396bd1c036a304df4ab052182e39da3410a6c82
TrueKuehli <rctcoaster2000@hotmail.de>
2018-09-07 19:14:05 +0200

    Improved getPointer function
    Removed overhead of turning the numbers to hexadecimal representation to concatenate them.

--- src/components/dataReaders/gen1/pointer.js
+++ src/components/dataReaders/gen1/pointer.js
@@ -1,5 +1,5 @@
 export default {
   'getPointer': function(lower, upper) {
-    return parseInt(upper.toString(16) + lower.toString(16), 16) % 0x4000;
+    return (upper * 0x100 + lower) % 0x4000;
   },
 };
